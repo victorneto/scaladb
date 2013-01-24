4d4f96c10fa0a8935c1bd57f803c50072424e387
Pedro Assumpção <pedro.assumpcao@gmail.com>
2013-01-23 22:44:10 -0600

    Updated rails to 3.2.11. Updated font-awesome and bootstrap-sass. Changed the main layout to be more flexible for using wide blocks for highlights. Created static pages for empresa, consultoria, laudos e projetos.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ ScaladbComBr::Application.routes.draw do
   root :to => "pages#index"
 
   get "empresa" => "pages#empresa", :as => "empresa"
+  get "projetos" => "pages#projetos", :as => "projetos"
+  get "consultoria" => "pages#consultoria", :as => "consultoria"
+  get "laudos" => "pages#laudos", :as => "laudos"
   get "contato" => "contacts#new", :as => "contact"
   resources :contacts, :only => [:create], :path => "contato/enviar"
 
