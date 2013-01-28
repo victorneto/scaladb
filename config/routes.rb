ea05bd7107e25ed3e361a3e9a94e6be3bc65276a
Pedro Assumpção <pedro.assumpcao@gmail.com>
2013-01-27 21:51:29 -0600

    Added pages for meio ambiente, acustica de edificacoes and industria.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ ScaladbComBr::Application.routes.draw do
   root :to => "pages#index"
 
   get "empresa" => "pages#empresa", :as => "empresa"
+  get "acustica-de-edificacoes" => "pages#acustica_de_edificacoes", :as => "acustica_de_edificacoes"
+  get "industria" => "pages#industria", :as => "industria"
+  get "meio-ambiente" => "pages#meio_ambiente", :as => "meio_ambiente"
   get "projetos" => "pages#projetos", :as => "projetos"
   get "consultoria" => "pages#consultoria", :as => "consultoria"
   get "laudos" => "pages#laudos", :as => "laudos"
