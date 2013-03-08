04087e8b42d2160e66f10284a03889cb66327f00
Pedro Assumpção <pedro.assumpcao@gmail.com>
2013-03-08 08:21:36 -0600

    Changed the destination email for contact form.

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -3,7 +3,7 @@ class ContactMailer < ActionMailer::Base
   def notification(contact)
     @contact = contact
     subject = "Contato Site"
-    mail(:to => "pedro.assumpcao@gmail.com", :subject => subject, :from => "#{@contact.name} <#{@contact.email}>") do |format|
+    mail(:to => "contato@scaladb.com.br", :subject => subject, :from => "#{@contact.name} <#{@contact.email}>") do |format|
       format.text { render "notification" }
       format.html { render "notification", :layout => "email" }
     end
