8615e2956ece2401ccb4b27c24fa25a56ffa54e4
Pedro Assumpção <pedro.assumpcao@gmail.com>
2013-03-12 08:24:49 -0500

    Added content for industry.

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,9 @@ ScaladbComBr::Application.routes.draw do
   get "acustica-de-edificacoes/consultoria" => "acustica_de_edificacoes#consultoria", :as => "acustica_de_edificacoes_consultoria"
   get "acustica-de-edificacoes/laudos" => "acustica_de_edificacoes#laudos", :as => "acustica_de_edificacoes_laudos"
   get "industria" => "industrias#index", :as => "industrias"
+  get "industria/projetos" => "industrias#projetos", :as => "industrias_projetos"
+  get "industria/consultoria" => "industrias#consultoria", :as => "industrias_consultoria"
+  get "industria/laudos" => "industrias#laudos", :as => "industrias_laudos"
   get "meio-ambiente" => "meio_ambientes#index", :as => "meio_ambientes"
 
   get "contato" => "contacts#new", :as => "contact"
